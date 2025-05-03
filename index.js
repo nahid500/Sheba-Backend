179d43dee332b10b7bc119684fce1a68512c61e6
Nahid Hossain <nahid95622@gmail.com>
2025-05-03 12:46:30 +0600

    updated port value

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000; 
+
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -932,121 +933,121 @@ app.get('/bookings', async(req,res)=>{
 
 })
 
-//sslcommerz init
-app.get('/pay/:amount/:trx_id', (req, res) => {
+// //sslcommerz init
+// app.get('/pay/:amount/:trx_id', (req, res) => {
 
 
-  const amount = Number(req.params.amount)
-  const trx_id = req.params.trx_id
+//   const amount = Number(req.params.amount)
+//   const trx_id = req.params.trx_id
 
-  const data = {
-      total_amount: amount,
-      currency: 'BDT',
-      tran_id: trx_id, 
-      success_url: `http://localhost:5000/success/${trx_id}`,
-      fail_url: `http://localhost:5000/fail/${trx_id}`,
-      cancel_url: `http://localhost:5000/cancel/${trx_id}`,
-      ipn_url: 'http://localhost:5000/ipn',
-      shipping_method: 'Courier',
-      product_name: 'Computer.',
-      product_category: 'Electronic',
-      product_profile: 'general',
-      cus_name: 'Customer Name',
-      cus_email: 'customer@example.com',
-      cus_add1: 'Dhaka',
-      cus_add2: 'Dhaka',
-      cus_city: 'Dhaka',
-      cus_state: 'Dhaka',
-      cus_postcode: '1000',
-      cus_country: 'Bangladesh',
-      cus_phone: '01711111111',
-      cus_fax: '01711111111',
-      ship_name: 'Customer Name',
-      ship_add1: 'Dhaka',
-      ship_add2: 'Dhaka',
-      ship_city: 'Dhaka',
-      ship_state: 'Dhaka',
-      ship_postcode: 1000,
-      ship_country: 'Bangladesh',
-  };
-  const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
-  sslcz.init(data).then(apiResponse => {
-      // Redirect the user to payment gateway
-      let GatewayPageURL = apiResponse.GatewayPageURL
-      res.redirect(GatewayPageURL)
-      console.log('Redirecting to: ', GatewayPageURL)
-  });
-})
+//   const data = {
+//       total_amount: amount,
+//       currency: 'BDT',
+//       tran_id: trx_id, 
+//       success_url: `http://localhost:5000/success/${trx_id}`,
+//       fail_url: `http://localhost:5000/fail/${trx_id}`,
+//       cancel_url: `http://localhost:5000/cancel/${trx_id}`,
+//       ipn_url: 'http://localhost:5000/ipn',
+//       shipping_method: 'Courier',
+//       product_name: 'Computer.',
+//       product_category: 'Electronic',
+//       product_profile: 'general',
+//       cus_name: 'Customer Name',
+//       cus_email: 'customer@example.com',
+//       cus_add1: 'Dhaka',
+//       cus_add2: 'Dhaka',
+//       cus_city: 'Dhaka',
+//       cus_state: 'Dhaka',
+//       cus_postcode: '1000',
+//       cus_country: 'Bangladesh',
+//       cus_phone: '01711111111',
+//       cus_fax: '01711111111',
+//       ship_name: 'Customer Name',
+//       ship_add1: 'Dhaka',
+//       ship_add2: 'Dhaka',
+//       ship_city: 'Dhaka',
+//       ship_state: 'Dhaka',
+//       ship_postcode: 1000,
+//       ship_country: 'Bangladesh',
+//   };
+//   const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+//   sslcz.init(data).then(apiResponse => {
+//       // Redirect the user to payment gateway
+//       let GatewayPageURL = apiResponse.GatewayPageURL
+//       res.redirect(GatewayPageURL)
+//       console.log('Redirecting to: ', GatewayPageURL)
+//   });
+// })
 
-app.post('/success/:trx_id', async (req,res) => {
-  const trx_id = req.params.trx_id
+// app.post('/success/:trx_id', async (req,res) => {
+//   const trx_id = req.params.trx_id
 
-  const paymentDetails = {
-    status: "success"
-  }
+//   const paymentDetails = {
+//     status: "success"
+//   }
 
-  await bookingsCollection.updateOne({
-    trx_id
-  },
-  {
-    $set: paymentDetails
-  },
-  (err,result) => {
-    err &&  res.send({
-      status: false,
-      message: err.message
-    })
-    result && res.redirect(`http://localhost:3000/dashboard`)
-  }
-)
-})
+//   await bookingsCollection.updateOne({
+//     trx_id
+//   },
+//   {
+//     $set: paymentDetails
+//   },
+//   (err,result) => {
+//     err &&  res.send({
+//       status: false,
+//       message: err.message
+//     })
+//     result && res.redirect(`http://localhost:3000/dashboard`)
+//   }
+// )
+// })
 
-app.post('/fail/:trx_id', async (req,res) => {
-  const trx_id = req.params.trx_id
+// app.post('/fail/:trx_id', async (req,res) => {
+//   const trx_id = req.params.trx_id
 
-  const paymentDetails = {
-    status: "failed"
-  }
+//   const paymentDetails = {
+//     status: "failed"
+//   }
 
-  await bookingsCollection.updateOne({
-    trx_id
-  },
-  {
-    $set: paymentDetails
-  },
-  (err,result) => {
-    err &&  res.send({
-      status: false,
-      message: err.message
-    })
-    result && res.redirect(`http://localhost:3000/dashboard`)
-  }
-)
-})
+//   await bookingsCollection.updateOne({
+//     trx_id
+//   },
+//   {
+//     $set: paymentDetails
+//   },
+//   (err,result) => {
+//     err &&  res.send({
+//       status: false,
+//       message: err.message
+//     })
+//     result && res.redirect(`http://localhost:3000/dashboard`)
+//   }
+// )
+// })
 
 
-app.post('/cancel/:trx_id', async (req,res) => {
-  const trx_id = req.params.trx_id
+// app.post('/cancel/:trx_id', async (req,res) => {
+//   const trx_id = req.params.trx_id
 
-  const paymentDetails = {
-    status: "cancel"
-  }
+//   const paymentDetails = {
+//     status: "cancel"
+//   }
 
-  await bookingsCollection.updateOne({
-    trx_id
-  },
-  {
-    $set: paymentDetails
-  },
-  (err,result) => {
-    err &&  res.send({
-      status: false,
-      message: err.message
-    })
-    result && res.redirect(`http://localhost:3000/dashboard`)
-  }
-)
-})
+//   await bookingsCollection.updateOne({
+//     trx_id
+//   },
+//   {
+//     $set: paymentDetails
+//   },
+//   (err,result) => {
+//     err &&  res.send({
+//       status: false,
+//       message: err.message
+//     })
+//     result && res.redirect(`http://localhost:3000/dashboard`)
+//   }
+// )
+// })
 
     // Start the server
     app.listen(port, () => {
